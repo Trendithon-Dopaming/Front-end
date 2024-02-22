9a34c362e3aece1437d24c2adb0449a8cbf041ea
westjuuu <westjiuuu@naver.com>
2024-02-22 22:27:46 +0900

    헤더 UI 수정

--- frontend1/src/components/header.js
+++ frontend1/src/components/header.js
@@ -21,11 +21,15 @@ function Header() {
             <path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C10.3431 4 9 5.34315 9 7C9 8.65685 10.3431 10 12 10C13.6569 10 15 8.65685 15 7C15 5.34315 13.6569 4 12 4ZM7 7C7 4.23858 9.23858 2 12 2C14.7614 2 17 4.23858 17 7C17 9.76142 14.7614 12 12 12C9.23858 12 7 9.76142 7 7Z" fill="white" />
           </svg>
           {isLoggedIn ? (
-            <a href="/myaccount">MY</a>
+            <a href="/my">MY</a>
           ) : (
-          <a href="/myaccount">로그인 | 회원가입</a>
+            <>
+              <a href="/Login" style={{ marginRight: '10px' }}>로그인</a>
+              <a href="/Join">회원가입</a>
+            </>
           )}
         </div>
+
       </div>
     </header>
   );
