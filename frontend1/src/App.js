69c5adb5798a82884c16acfe924f0030896d14a7
westjuuu <westjiuuu@naver.com>
2024-02-23 19:20:25 +0900

    로그인 상태 구현

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { AuthProvider } from './components/AuthContext'; //인증상태 관리
 //페이지
 import Mainpage from "./pages/mainpage";
 
 function App() {
     return (
         <div className="App">
-            <Router>
-                <Routes>
-                    <Route path="/mainpage" element={<Mainpage />}></Route>
-                </Routes>
-            </Router>
+            <AuthProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/mainpage" element={<Mainpage />}></Route>
+                    </Routes>
+                </Router>
+            </AuthProvider>
         </div>
     );
 }
