62835bc785d0cad577ad4df9c5a57b2c68f5d6ff
doyeon <cat0806@naver.com>
2024-02-24 03:25:13 +0900

    이름 수정 기능 추가

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -3,17 +3,20 @@ import Mainpage from "./pages";
 import Login from "./pages/login";
 import Join from "./pages/join";
 import Mypage from "./pages/mypage";
+import { AuthProvider } from "./components/AuthContext";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/mypage" element={<Mypage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/mypage" element={<Mypage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 export default App;
--- frontend1/src/pages/mypage/index.js
+++ frontend1/src/pages/mypage/index.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import EditIcon from "../../assets/editIcon.svg";
+//import PasswordModal from "./pwChangeModal";
+import { useAuth } from "../../components/AuthContext";
 
 const MypageSection = styled.section`
   margin: 0 auto 0;
@@ -11,7 +13,7 @@ const MypageSection = styled.section`
   .acntBody {
     font-family: Pretendard;
     overflow: hidden;
-    padding: 108px 200px 234px 200px;
+    padding: 108px 200px 234px 180px;
   }
   .title {
     color: #fffbfb;
@@ -28,12 +30,12 @@ const MypageSection = styled.section`
     font-weight: 500;
     line-height: normal;
     margin-bottom: 30px;
-    .icon {
-      margin-left: 10px;
+    img {
+      cursor: pointer;
     }
   }
   .infoField {
-    width: 1110px;
+    width: 100%;
     height: 62px;
     flex-shrink: 0;
     border: 1px solid #9b9b9b;
@@ -44,74 +46,132 @@ const MypageSection = styled.section`
     font-weight: 500;
     line-height: normal;
     margin-bottom: 30px;
-    padding-left: 16px;
+    padding-left: 20px;
     display: flex;
     align-items: center;
   }
+  input {
+    border: none;
+    color: #9b9b9b;
+  }
   .saveBtn {
-    width: 1126px;
+    width: 100%;
     height: 58px;
     flex-shrink: 0;
-    background: #4e4e4e;
-    color: #2b2a2a;
+    background: ${(props) => (props.modified ? "#fff" : "#4e4e4e")};
+    color: ${(props) => (props.modified ? "#000" : "#2b2a2a")};
     font-size: 30px;
     font-style: normal;
     font-weight: 500;
     line-height: normal;
     margin-top: 96px;
+    padding: 0 10px;
     display: flex;
     justify-content: center;
     align-items: center;
+    cursor: ${(props) => (props.modified ? "pointer" : "default")};
   }
 `;
 
 const Mypage = () => {
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [nameInput, setNameInput] = useState("");
+  const [originalName, setOriginalName] = useState("");
+  const [isNameModified, setIsNameModified] = useState(false);
+  const { isLoggedIn } = useAuth();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    const headers = {
-      Authorization: `Token ${token}`,
-    };
-
     axios
-      .get("http://127.0.0.1:8000/user/profile/", {
+      .get("http://localhost:8080/user/info", {
         withCredentials: true,
-        headers: headers,
       })
       .then((response) => {
-        const { profile, scores } = response.data;
-        setUserData({ profile, scores });
+        setUserData(response.data);
+        setNameInput(response.data.name);
+        setOriginalName(response.data.name);
         setLoading(false);
       })
       .catch((error) => {
-        console.error(error);
+        console.error("유저 정보를 가져오는 중 에러 발생:", error);
         setLoading(false);
       });
   }, []);
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleNameChange = (e) => {
+    setNameInput(e.target.value);
+    setIsNameModified(e.target.value !== originalName);
+  };
+
+  const handleSave = () => {
+    axios
+      .patch(
+        "http://localhost:8080/user/edit/info",
+        { name: nameInput },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        if (response.data === "success") {
+          setUserData((prevUserData) => ({
+            ...prevUserData,
+            name: nameInput,
+          }));
+          setOriginalName(nameInput);
+          setIsModalOpen(false);
+          setIsNameModified(false);
+        } else {
+          console.error("이름 변경에 실패하였습니다.");
+        }
+      })
+      .catch((error) => {
+        console.error("이름 변경 요청 중 에러 발생:", error);
+      });
+  };
+
   return (
-    <MypageSection>
+    <MypageSection modified={isNameModified}>
       <div className="acntBody">
         {loading ? (
-          <p>데이터를 불러오는 중입니다...</p>
-        ) : (
+          <p>데이터를 불러오는 중입니다.</p>
+        ) : isLoggedIn && userData ? (
           <div>
             <div className="title">나의 정보</div>
             <div className="subtitle">이름</div>
-            <div className="infoField">db에서 불러오기</div>
+            <input
+              type="text"
+              className="infoField"
+              value={nameInput}
+              onChange={handleNameChange}
+            />
             <div className="subtitle">이메일</div>
-            <div className="infoField">db에서 불러오기</div>
+            <div className="infoField">{userData.email}</div>
             <div className="subtitle">
               비밀번호 &nbsp;
-              <img src={EditIcon} alt="edit icon" />
+              {/*<img src={EditIcon} alt="edit icon" onClick={openModal} />*/}
             </div>
-
-            <div className="infoField">db에서 불러오기</div>
-            <div className="saveBtn">저장하기</div>
+            <div className="infoField">****</div>
+            <div
+              className="saveBtn"
+              onClick={isNameModified ? handleSave : undefined}
+            >
+              저장하기
+            </div>
           </div>
+        ) : (
+          <div className="subtitle">로그인이 필요합니다.</div>
         )}
+        {isModalOpen && <PasswordModal closeModal={closeModal} />}
       </div>
     </MypageSection>
   );
